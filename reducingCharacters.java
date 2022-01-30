aa792776bde1d4916f4b35fc18016f5aee788627
GouthamKamath <61709697+GouthamKamath@users.noreply.github.com>
2022-01-30 13:21:56 +0530

    Create reducingCharacters.java

---
+++ reducingCharacters.java
@@ -0,0 +1,30 @@
+package com.headfirst;
+
+import java.util.ArrayList;
+
+public class Interview {
+	
+	public String recedingChars(String s) {
+		String b="";
+		String[] al = s.split("");
+		int count = al.length;
+		int index=0;
+		while(count>0) {
+			for(int i=0;i<count;i++) {
+				b+=al[index];
+			}
+			index++;
+			count--;
+		}
+		return b;
+	}
+  
+	public static void main(String[] args) {
+		String a ="gouthamkamath";
+		Interview i=new Interview();
+		String b = i.recedingChars(a);
+		System.out.println(b);
+
+	}
+
+}
