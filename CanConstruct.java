af29000c0739c4adaa7338e35cee7c10cd0bebe2
GouthamKamath <61709697+GouthamKamath@users.noreply.github.com>
2022-03-19 17:33:57 +0530

    Update CanConstruct.java

--- CanConstruct.java
+++ CanConstruct.java
@@ -1,4 +1,30 @@
-class CanConstruct {
+/* Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters. */
+
+
+class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         boolean canConstruct = false;
        
@@ -16,3 +42,4 @@ class CanConstruct {
         return canConstruct;
     }
 }
+
